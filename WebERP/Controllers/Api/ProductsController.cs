11d32c66f111651bddfc7b68dc342dfdb4098d8e
gustavoosantoos <gustavoosantoos@outlook.com>
2018-06-30 19:04:30 -0300

    Implementação inicial do cálculo de acurácia de estoque e endpoint para atualizar quantidade de produto

---
+++ WebERP/Controllers/Api/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebERP.Data.Repositories;
+using WebERP.Models;
+
+namespace WebERP.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : BaseController
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductsController(
+            CurrentUtils current,
+            ProductRepository productRepository) : base(current)
+        {
+            this._productRepository = productRepository;
+        }
+
+        [Authorize(Roles = "SuperAdmin, SupervisorDeEstoque")]
+        [HttpPost]
+        [Route("AtualizarQuantidade/{id}/{novaQuantidade}")]
+        public IActionResult AtualizarQuantidade(int id, double novaQuantidade)
+        {
+            var produto = this._productRepository.FindById(id);
+            if (produto == null)
+                return NotFound();
+
+            produto.Quantidade = novaQuantidade;
+            this._productRepository.Save(produto);
+            return Ok();
+        }
+    }
+}
