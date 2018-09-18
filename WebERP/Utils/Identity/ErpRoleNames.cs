7941ebe312c290a31c8df15aafeecae21d9674b0
Gustavo Santos <gustavo.dev@outlook.com.br>
2018-09-18 13:04:20 -0300

    Declaração das roles de vendas e permissão no menu lateral

--- WebERP/Utils/Identity/ErpRoleNames.cs
+++ WebERP/Utils/Identity/ErpRoleNames.cs
@@ -11,21 +11,28 @@
 
         public const string SupervisorDeEstoque = "SupervisorDeEstoque";
         public const string Estoque = "Estoque";
+
+        public const string SupervisorDeVendas = "SupervisorDeVendas";
+        public const string Vendedor = "Vendedor";
     }
 
     public static class ErpRoleGroups
     {
-        public const string Estoque 
-            = ErpRoleNames.SuperAdmin + "," + ErpRoleNames.SupervisorDeEstoque +"," + ErpRoleNames.Estoque;
+        public const string Estoque = 
+            ErpRoleNames.SuperAdmin + "," + ErpRoleNames.SupervisorDeEstoque +"," + ErpRoleNames.Estoque;
 
-        public const string Compras
-            = ErpRoleNames.SuperAdmin + "," + ErpRoleNames.SupervisorDeCompras + "," + ErpRoleNames.Compras;
+        public const string Compras =
+            ErpRoleNames.SuperAdmin + "," + ErpRoleNames.SupervisorDeCompras + "," + ErpRoleNames.Compras;
 
-        public const string Administrativo 
-            = ErpRoleNames.SuperAdmin + "," + ErpRoleNames.Administrativo;
+        public const string Administrativo = 
+            ErpRoleNames.SuperAdmin + "," + ErpRoleNames.Administrativo;
+
+        public const string Vendas =
+            ErpRoleNames.SuperAdmin + "," + ErpRoleNames.SupervisorDeVendas + "," + ErpRoleNames.Vendedor;
 
         public static string[] RolesEstoque => new[] { ErpRoleNames.SuperAdmin, ErpRoleNames.SupervisorDeEstoque, ErpRoleNames.Estoque};
         public static string[] RolesCompras => new[] { ErpRoleNames.SuperAdmin, ErpRoleNames.SupervisorDeCompras, ErpRoleNames.Compras};
         public static string[] RolesAdministrativas => new[] { ErpRoleNames.SuperAdmin, ErpRoleNames.Administrativo};
+        public static string[] RolesVendas => new[] { ErpRoleNames.SuperAdmin, ErpRoleNames.SupervisorDeVendas, ErpRoleNames.Vendedor };
     }
 }
